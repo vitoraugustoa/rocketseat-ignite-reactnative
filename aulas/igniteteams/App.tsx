c4a4d5d5cdad750f007d6d72a172f579d99ee289
vitor.alopes <vitor.alopes@agnet.com.br>
2024-05-09 19:01:19 -0300

    Atualizando.

---
+++ aulas/igniteteams/App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import theme from "./src/theme";
+
+import { Loading } from "@components/Loading";
+
+import { Groups } from "@screens/Groups";
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Groups /> : <Loading />}
+      </>
+    </ThemeProvider>
+  );
+}
